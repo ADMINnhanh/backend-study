6e18021e1873e50b78f0dda6c71416e14fe0443b
nhanh <3201595386@qq.com>
2024-12-19 11:00:00 +0800

    初始化 Java 学习项目

---
+++ src/package01/Class01.java
@@ -0,0 +1,9 @@
+package package01;
+
+public class Class01 {
+    public static void main(String[] args) {
+        int count = 20;
+        int price = 15;
+        System.out.print(count * price);
+    }
+}
